892e2d71e424efd002a4557b77529f6f5dc3402a
narustudio <slackjawed12@gmail.com>
2023-08-18 22:53:55 +0900

    chore(server): jest 추가

---
+++ server/src/controllers/user.router.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { CURRENT_API_VERSION } from "../../../common/constants";
+
+const router = express.Router();
+
+export default (app: Application) => {
+  app.use(`/${CURRENT_API_VERSION}/users`, router);
+
+  router.get("/", async (req, res, next) => {
+    try {
+    } catch (e) {
+      next(e);
+    }
+  });
+};
