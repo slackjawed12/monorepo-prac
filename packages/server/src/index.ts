c4d0873c33be89db349a99b7519f7e5a07a0b63d
Tony Lee <tony@treasurer.co.kr>
2023-08-23 13:55:57 +0900

    feautre(all): turborepo 설치

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,10 @@
 import { map, isNil } from "lodash";
 import { CURRENT_API_VERSION } from "@monoprac/common";
+import { resolve } from "path";
 const app = Array.from(Array(10), (_) => 0);
 
 (async () => {
+  console.log(resolve(__dirname, "../../../../.pnp.cjs"));
   console.log(CURRENT_API_VERSION);
   console.log(isNil("3457"));
   console.log(map(app, (val) => 1));
